b633a30805e7e547341add01d354f5c29c20531a
Ermolenko Danil <danil-ermolenko-01@mail.ru>
2023-11-13 22:55:08 +0300

    kafka with amigoscode

---
+++ src/main/java/danilermolenko/kafka/MessageRequest.java
@@ -0,0 +1,4 @@
+package danilermolenko.kafka;
+
+public record MessageRequest(String message) {
+}
---
+++ src/main/java/danilermolenko/kafka/listeners/KafkaListeners.java
@@ -0,0 +1,13 @@
+package danilermolenko.kafka.listeners;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(topics = "danilsTopic", groupId = "groupId")
+    void listener(String data){
+        System.out.println("Listener has received " + data);
+    }
+}
